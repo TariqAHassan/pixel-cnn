96135522710c423e63277a7420cb5f8b76ef8c79
TariqAHassan <tariqaahassan@gmail.com>
2017-07-17 08:05:42 -0300

    Refactor

--- train.py
+++ train.py
@@ -23,40 +23,46 @@ import data.imagenet_data as imagenet_data
 
 # -----------------------------------------------------------------------------
 
-parser = argparse.ArgumentParser()
+args_dict = {
+    # Data I/O ---
+    'data_dir': '/tmp/pxpp/data',           # Location of the dataset
+    'save_dir': '/tmp/pxpp/save',           # Location for parameter checkpoints and samples
+    'data_set': 'cifar',                    # Can be either cifar|imagenet
+    'save_interval': 20,                    # Every how many epochs to write checkpoint/samples.
+    'load_params': False,                   # Whether or not to restore training from previous model checkpoint.
 
-# Data I/O ---
-parser.add_argument('-i', '--data_dir', type=str, default='/tmp/pxpp/data', help='Location for the dataset')
-parser.add_argument('-o', '--save_dir', type=str, default='/tmp/pxpp/save',  help='Location for parameter checkpoints and samples')
-parser.add_argument('-d', '--data_set', type=str, default='cifar', help='Can be either cifar|imagenet')
-parser.add_argument('-t', '--save_interval', type=int, default=20, help='Every how many epochs to write checkpoint/samples?')
-parser.add_argument('-r', '--load_params', dest='load_params', action='store_true', help='Restore training from previous model checkpoint?')
+    # Model ---
+    'nr_resnet': 5,                         # Number of residual blocks per stage of the model
+    'nr_filters': 160,                      # Number of filters to use across the model. Higher = larger model.
+    'nr_logistic_mix': 10,                  # Number of logistic components in the mixture. Higher = more flexible model
+    'resnet_nonlinearity': 'concat_elu',    # Nonlinearity to use in the ResNet layers; 'concat_elu', 'elu' or 'relu'.
+    'class_conditional': False,             # Condition generative model on labels?
 
-# Model ---
-parser.add_argument('-q', '--nr_resnet', type=int, default=5,  help='Number of residual blocks per stage of the model')
-parser.add_argument('-n', '--nr_filters', type=int, default=160, help='Number of filters to use across the model. Higher = larger model.')
-parser.add_argument('-m', '--nr_logistic_mix', type=int, default=10, help='Number of logistic components in the mixture. Higher = more flexible model')
-parser.add_argument('-z', '--resnet_nonlinearity', type=str, default='concat_elu', help='Which nonlinearity to use in the ResNet layers. One of "concat_elu", "elu", "relu" ')
-parser.add_argument('-c', '--class_conditional', dest='class_conditional', action='store_true', help='Condition generative model on labels?')
+    # Optimization ---
+    'learning_rate': 0.001,                 # Base learning rate
+    'lr_decay': 0.999995,                   # Learning rate decay, applied every step of the optimization
+    'batch_size': 12,                       # Batch size during training per GPU
+    'init_batch_size': 100,                 # How much data to use for data-dependent initialization
+    'dropout_p': 0.5,                       # Dropout strength (1 - keep probability)
+    'max_epochs': 5000,                     # Number of epochs to run in total.
+    'nr_gpu': 1,                            # Number of GPUs
 
-# Optimization ---
-parser.add_argument('-l', '--learning_rate', type=float, default=0.001, help='Base learning rate')
-parser.add_argument('-e', '--lr_decay', type=float, default=0.999995, help='Learning rate decay, applied every step of the optimization')
-parser.add_argument('-b', '--batch_size', type=int, default=12, help='Batch size during training per GPU')
-parser.add_argument('-a', '--init_batch_size', type=int, default=100, help='How much data to use for data-dependent initialization.')
-parser.add_argument('-p', '--dropout_p', type=float, default=0.5, help='Dropout strength (i.e. 1 - keep_prob). 0 = No dropout, higher = more dropout.')
-parser.add_argument('-x', '--max_epochs', type=int, default=5000, help='How many epochs to run in total?')
-parser.add_argument('-g', '--nr_gpu', type=int, default=8, help='How many GPUs to distribute the training across?')
+    # Evaluation ---
+    'polyak_decay': 0.9995,                 # Exponential decay rate of the sum of previous model iterates during
+                                            # Polyak averaging
 
-# Evaluation ---
-parser.add_argument('--polyak_decay', type=float, default=0.9995, help='Exponential decay rate of the sum of previous model iterates during Polyak averaging')
+    # Reproducibility ---
+    'seed': 1                               # Random seed to use.
+}
 
-# Reproducibility ---
-parser.add_argument('-s', '--seed', type=int, default=1, help='Random seed to use')
 
 # Parse ---
-args = parser.parse_args()
-print('input args:\n', json.dumps(vars(args), indent=4, separators=(',', ':')))  # pretty print args
+class ArgsDictWrapper(object):
+    def __init__(self, args_dict):
+        self.__dict__.update(args_dict)
+
+args = ArgsDictWrapper(args_dict)
+
 
 # -----------------------------------------------------------------------------
 # fix random seed for reproducibility
